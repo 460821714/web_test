4b22afe4df0d45e00fec3d75ba14fcfcd52022cc
gm <460821714@qq.com>
2018-08-17 15:03:19 +0800

    first page

---
+++ webserver/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func appHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "<h1>hello,my name is Jake.</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", appHandler)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
